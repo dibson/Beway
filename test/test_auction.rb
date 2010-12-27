8c6f7915baa0d5e102b5b091995885997dc62b5a
Dibson T Hoffweiler <consulting@hoffweiler.com>
2010-12-26 17:18:35 -0800

    In auction tests, keep an array of valid auctions, and programmatically describe them for rspec.

--- test/test_auction.rb
+++ test/test_auction.rb
@@ -2,11 +2,37 @@ require_relative '../lib/beway/auction'
 
 HTML_DIR = File.dirname(__FILE__) + File::SEPARATOR + 'html' + File::SEPARATOR
 
-AUCTION_MENS_CARDIGAN_DUTCH_BIN = {
-  :url => HTML_DIR + 'mens-cardigans-duthch-bin.html',
-}
+AUCTIONS_VALID = []
+AUCTIONS_INVALID = []
 
-AUCTION_PINK_SWEATER_BID_BIN = {
+AUCTIONS_VALID << {
+  :url => HTML_DIR + 'pink-sweater-bid-bin.html',
+  :description => 'ALFANI  MENS SWEATER PINK SMALL  NEW WITH TAGS',
+  :current_bid => 'US $14.99',
+  :min_bid => 'US $14.99',
+  :time_left => '3h 28m 33s',
+  :end_time => 'Dec 15, 2010 10:10:36 PST',
+  :auction_number => '250740721413'
+}
+AUCTIONS_VALID << {
+  :url => HTML_DIR + 'polo-lambs-wool.html',
+  :description => 'NEW POLO RALPH LAUREN SWEATER MENS SMALL S LAMBSWOOL',
+  :current_bid => 'US $27.00',
+  :min_bid => 'US $28.00',
+  :time_left => '3h 35m 33s',
+  :end_time => 'Dec 15, 2010 10:20:09 PST',
+  :auction_number => '380297207180'
+}
+AUCTIONS_VALID << {
+  :url => HTML_DIR + 'xmas-sweater.html',
+  :current_bid => 'US $5.00',
+  :min_bid => 'US $5.50',
+  :description => 'UGLY CHRISTMAS BIG BALLS SWEATER SZ S/M',
+  :time_left => '1h 2m 44s',
+  :end_time => 'Dec 15, 2010 07:42:16 PST',
+  :auction_number => '270680279290'
+}
+AUCTIONS_VALID << {
   :url => HTML_DIR + 'pink-sweater-bid-bin.html',
   :description => 'ALFANI  MENS SWEATER PINK SMALL  NEW WITH TAGS',
   :current_bid => 'US $14.99',
@@ -16,29 +42,14 @@ AUCTION_PINK_SWEATER_BID_BIN = {
   :auction_number => '250740721413'
 }
 
-AUCTION_SPRING_MERCER_BIN_MO = {
+AUCTIONS_INVALID << {
+  :url => HTML_DIR + 'mens-cardigans-duthch-bin.html',
+}
+
+AUCTIONS_INVALID << {
   :url => HTML_DIR + 'spring-mercer-bin-mo.html',
 }
 
-AUCTION_POLO_LAMBS_WOOL = {
-  :url => HTML_DIR + 'polo-lambs-wool.html',
-  :description => 'NEW POLO RALPH LAUREN SWEATER MENS SMALL S LAMBSWOOL',
-  :current_bid => 'US $27.00',
-  :min_bid => 'US $28.00',
-  :time_left => '3h 35m 33s',
-  :end_time => 'Dec 15, 2010 10:20:09 PST',
-  :auction_number => '380297207180'
-}
-
-AUCTION_XMAS_SWEATER = {
-  :url => HTML_DIR + 'xmas-sweater.html',
-  :current_bid => 'US $5.00',
-  :min_bid => 'US $5.50',
-  :description => 'UGLY CHRISTMAS BIG BALLS SWEATER SZ S/M',
-  :time_left => '1h 2m 44s',
-  :end_time => 'Dec 15, 2010 07:42:16 PST',
-  :auction_number => '270680279290'
-}
 
 describe Beway::Auction do
 
@@ -98,24 +109,12 @@ describe Beway::Auction do
 
   end
 
-  describe "valid auction - x-mas sweater" do
-    it_should_behave_like "a valid auction" do
-      let(:auction) { Beway::Auction.new(AUCTION_XMAS_SWEATER[:url]) }
-      let(:data) { AUCTION_XMAS_SWEATER }
-    end
-  end
-
-  describe "valid auction - polo lambs wool" do
-    it_should_behave_like "a valid auction" do
-      let(:auction) { Beway::Auction.new(AUCTION_POLO_LAMBS_WOOL[:url]) }
-      let(:data) { AUCTION_POLO_LAMBS_WOOL }
-    end
-  end
-
-  describe "valid auction - pink sweater w/ bin option" do
-    it_should_behave_like "a valid auction" do
-      let(:auction) { Beway::Auction.new(AUCTION_PINK_SWEATER_BID_BIN[:url]) }
-      let(:data) { AUCTION_PINK_SWEATER_BID_BIN }
+  AUCTIONS_VALID.each do |a|
+    describe "valid auction - #{a[:description]}" do
+      it_should_behave_like "a valid auction" do
+        let(:auction) { Beway::Auction.new(a[:url]) }
+        let(:data) { a }
+      end
     end
   end
 
