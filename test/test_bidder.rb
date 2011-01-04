d6386d15f08e2fe46ad1143b0ef3292b6c06ec05
Dibson T Hoffweiler <consulting@hoffweiler.com>
2011-01-03 17:40:21 -0800

    When testing bidder, show a message if test failed due to bad login

--- test/test_bidder.rb
+++ test/test_bidder.rb
@@ -1,4 +1,5 @@
 require_relative '../lib/beway/bidder'
+require_relative './config'
 
 describe Beway::Bidder do
 
@@ -16,6 +17,13 @@ describe Beway::Bidder do
       bidder = Beway::Bidder.new(VALID_CREDS[:username], VALID_CREDS[:password])
       bidder.logged_in.should be_false
       bidder.login
+
+      if not bidder.logged_in
+        puts "\n" + "*" * 78
+        puts "Did you set your username and password in test/config.rb?"
+        puts "You should do so to get this test to pass"
+        puts "*" * 78
+      end
       bidder.logged_in.should be_true
     end
   end
